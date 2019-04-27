c2e3926b378a473cb5105aec06a9a733796143dc
Dirk Lemstra <dirk@lemstra.org>
2019-04-27 08:53:04 +0200

    Fixed header file

--- src/libjasper/include/jasper/jas_dll.h
+++ src/libjasper/include/jasper/jas_dll.h
@@ -1,21 +1,15 @@
 #ifndef JAS_DLL_H
 #define JAS_DLL_H
 
-#if defined(JAS_DLL)
-  # if defined(_DLL) && !defined(_LIB)
-  #   if !defined(JAS_WIN_MSVC_BUILD)
-  #     define JAS_DLLEXPORT __declspec(dllimport)
-  #   else
-  #     define JAS_DLLEXPORT __declspec(dllexport)
-  #   endif
-  # else
-  #   define JAS_EXPORT
-  # endif
-  # define JAS_DLLLOCAL
-
-#else
-	#define JAS_DLLEXPORT
-	#define JAS_DLLLOCAL
-#endif
+# if defined(_DLL) && !defined(_LIB)
+#   if !defined(JAS_WIN_MSVC_BUILD)
+#     define JAS_DLLEXPORT __declspec(dllimport)
+#   else
+#     define JAS_DLLEXPORT __declspec(dllexport)
+#   endif
+# else
+#   define JAS_DLLEXPORT
+# endif
+# define JAS_DLLLOCAL
 
 #endif
